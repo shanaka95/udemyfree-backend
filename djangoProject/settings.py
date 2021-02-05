c561e8a0e16b007ad1beb11e62bf741e95cc8919
root <root@udemyfree.courses>
2021-02-05 05:56:18 +0000

    server

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = 'static'
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20
